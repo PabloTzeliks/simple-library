4825cf7769bfcb9478ac3a9100fefdcf6c57c4f0
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-03 19:59:57 -0300

    feat: create a connection JDBC class

---
+++ src/main/java/pablo/tzeliks/infraestructure/Conn/Conexao.java
@@ -0,0 +1,16 @@
+package pablo.tzeliks.infraestructure.Conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca_db?useSSL=false&amp;serverTimezone=UTC";
+    private static final String User = "root";
+    private static final String Passwd = "passwd";
+
+    public static Connection conexao() throws SQLException {
+        return DriverManager.getConnection(URL, User, Passwd);
+    }
+}
---
+++ src/main/java/pablo/tzeliks/infraestructure/LivroRepositoy.java
@@ -0,0 +1,4 @@
+package pablo.tzeliks.infraestructure;
+
+public class LivroRepositoy {
+}
