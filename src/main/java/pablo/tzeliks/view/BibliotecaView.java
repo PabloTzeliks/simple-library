e1e97069088090e0cf9dbfba8db1bc1df11a1932
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-03 20:26:05 -0300

    feat: creates the main menu view

--- src/main/java/pablo/tzeliks/view/BibliotecaView.java
+++ src/main/java/pablo/tzeliks/view/BibliotecaView.java
@@ -1,6 +1,12 @@
 package pablo.tzeliks.view;
 
+import pablo.tzeliks.view.helper.MenuHelper;
+
 public class BibliotecaView {
 
+    while (true) {
+
+        MenuHelper.
+    }
 
 }
--- src/main/java/pablo/tzeliks/view/helper/MenuHelper.java
+++ src/main/java/pablo/tzeliks/view/helper/MenuHelper.java
@@ -7,4 +7,15 @@ public class MenuHelper {
         System.out.println("\n" + "=".repeat(50) + "\n");
     }
 
+    public static void menuPrincipal() {
+
+        espacador();
+
+        System.out.println("=== MENU PRINCIPAL ===");
+        System.out.println("\n1- Registrar Livro");
+        System.out.println("\n2- Registrar um empréstimo");
+        System.out.println("\n3- Registrar uma Devolução");
+        System.out.println("\n4- Listar Livros");
+        System.out.println("\n5- Listar Emprestimos");
+    }
 }
