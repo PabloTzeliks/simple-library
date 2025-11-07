86c868e89468849f928d478fe8f1f7775f50bf6f
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-06 21:48:38 -0300

    feat: implements the query for Emprestimo's listing

--- src/main/java/pablo/tzeliks/infraestructure/EmprestimoRepository.java
+++ src/main/java/pablo/tzeliks/infraestructure/EmprestimoRepository.java
@@ -2,11 +2,11 @@ package pablo.tzeliks.infraestructure;
 
 import pablo.tzeliks.infraestructure.Conn.Conexao;
 import pablo.tzeliks.model.Emprestimo;
+import pablo.tzeliks.model.Livro;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmprestimoRepository {
 
@@ -24,6 +24,30 @@ public class EmprestimoRepository {
             stmt.setDate(3, Date.valueOf(emprestimo.getDataDevolucao()));
 
             stmt.executeUpdate();
+        }
+    }
+
+    public List<Emprestimo> list() throws SQLException {
+
+        List<Emprestimo> emprestimos = new ArrayList<>();
+
+        String sqlListagemEmprestimos = """
+                SELECT id, livro_id, data_emprestimo, data_devolucao FROM emprestimos"
+                """;
+
+        try(Connection conn = Conexao.conexao();
+            PreparedStatement stmt = conn.prepareStatement(sqlListagemEmprestimos)) {
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                Emprestimo emprestimo = new Emprestimo(rs.getInt("id"), new Livro(rs.getInt("livro_id"), null, null, 0, false), rs.getDate("data_emprestimo").toLocalDate(), rs.getDate("data_devolucao").toLocalDate());
+
+                emprestimos.add(emprestimo);
+            }
+
+            return emprestimos;
         }
     }
 }
